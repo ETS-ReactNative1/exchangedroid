23c2fc8a64a6a762218877ebcb636ee7f8d01fed
enesozturk <enesozturk.d@gmail.com>
2019-12-04 18:16:04 +0300

    application interface

--- App.js
+++ App.js
@@ -10,104 +10,106 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
+  TextInput,
+  View,
   StatusBar,
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const APP_BACKGROUND_COLOR = '#5bdfc3';
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+      <SafeAreaView style={styles.safeAreaView}>
+        <LinearGradient
+          colors={['#5bdfc3', '#2e9696']}
+          style={styles.container}>
+          <View style={styles.content}>
+            <Text style={styles.title}>Exchange</Text>
           </View>
-        </ScrollView>
+          <View style={styles.flexItem}>
+            <View style={styles.currencyIcon}>
+              <Text style={styles.currencyText}>TR</Text>
+            </View>
+            <TextInput value="0.00" style={styles.textarea} />
+          </View>
+          <View style={styles.flexItem}>
+            <View style={styles.exchangeIcon}></View>
+            <TextInput value="0.00" style={styles.textarea} />
+          </View>
+          <View style={styles.flexItem}>
+            <View style={styles.currencyIcon}>
+              <Text style={styles.currencyText}>USD</Text>
+            </View>
+            <TextInput value="0.00" style={styles.textarea} />
+          </View>
+        </LinearGradient>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  safeAreaView: {backgroundColor: APP_BACKGROUND_COLOR},
+  container: {
+    height: '100%',
+    backgroundColor: APP_BACKGROUND_COLOR,
+    display: 'flex',
+    flexDirection: 'column',
+    paddingVertical: 60,
+    paddingTop: 20,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  content: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  body: {
-    backgroundColor: Colors.white,
+  title: {fontSize: 35, color: 'white', marginBottom: 30},
+  flexItem: {
+    flex: 1,
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  currencyIcon: {
+    width: 80,
+    height: 80,
+    backgroundColor: 'white',
+    borderRadius: 40,
+    marginRight: 20,
+    borderWidth: 5,
+    borderColor: 'rgba(0,0,0,.1)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  currencyText: {
+    fontSize: 22,
+    color: 'gray',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  exchangeIcon: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'white',
+    borderRadius: 30,
+    marginRight: 20,
+    borderWidth: 5,
+    borderColor: 'rgba(0,0,0,.1)',
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
+  textarea: {
+    width: 200,
+    fontSize: 35,
+    padding: 5,
     textAlign: 'right',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.5)',
   },
 });
 
